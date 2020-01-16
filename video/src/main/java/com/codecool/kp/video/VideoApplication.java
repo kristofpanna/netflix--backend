14d70d7f515fdbfe9606e0f0369044d5ada58db9
Panna Kristóf <kristofpanna@gmail.com>
2020-01-16 17:39:30 +0100

    Add service-registry module with eureka; make video application available

--- video/src/main/java/com/codecool/kp/video/VideoApplication.java
+++ video/src/main/java/com/codecool/kp/video/VideoApplication.java
@@ -2,7 +2,7 @@ package com.codecool.kp.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-//@EnableEurekaClient
+@EnableEurekaClient
 public class VideoApplication {
 
 	public static void main(String[] args) {
